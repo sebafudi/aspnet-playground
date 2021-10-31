80612a9131d7169476e60a88d1e3dc085e0657e0
sebafudi <sebastianfudalej@gmail.com>
2021-10-31 20:34:01 +0100

    feat: change text inside index

--- Program.cs
+++ Program.cs
@@ -6,8 +6,7 @@ builder.Services.AddRazorPages();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
+if (!app.Environment.IsDevelopment()) {
     app.UseExceptionHandler("/Error");
 }
 app.UseStaticFiles();
